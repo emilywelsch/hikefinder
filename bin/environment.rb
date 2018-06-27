8f5dce5b8f450543354a5e3b8efcab68a951b742
Emily Welsch <emwelsch@gmail.com>
2018-06-27 14:26:58 +0000

    created/updated bin file

---
+++ bin/environment.rb
@@ -0,0 +1,10 @@
+require 'pry'
+require 'nokogiri'
+require 'open-uri'
+require 'require_all'
+require 'colorize'
+
+require_relative '../lib/hikefinder/version.rb'
+require_relative '../lib/cli.rb'
+require_relative '../lib/trail.rb'
+require_relative '../lib/scraper.rb'
